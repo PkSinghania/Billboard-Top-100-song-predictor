099e0c89a761af86dbf634b3b507edd3a4173f4a
PkSinghania <pratyushsinghania29@gmail.com>
2020-07-07 21:24:36 +0530

    preapring final data

--- combine_csv.py
+++ combine_csv.py
@@ -9,5 +9,7 @@ billboard_hits = df1[['Title','Artist','Featuring','duration_ms','acousticness',
 billboard_non_hits = df2[['Title','Artist','Featuring','duration_ms','acousticness','danceability','energy','instrumentalness','key','liveness','loudness','speechiness','tempo','valence','Hit']]
 
 final_data = pd.concat([billboard_hits, billboard_non_hits], axis=0)
+final_data = final_data.drop_duplicates(subset=["Title", "Artist"], keep="first")
 
 final_data.to_csv("final_data.csv", index = False)
+
